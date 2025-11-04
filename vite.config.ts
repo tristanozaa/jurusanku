90be7899da938c0b61651d19d83273c993000d3c
tristanozaa <tristanozaa@gmail.com>
2025-11-04 21:33:56 +0700

    netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import vercel from "vite-plugin-vercel";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
+import netlify from "@netlify/vite-plugin";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
@@ -10,8 +10,7 @@ export default defineConfig(({ mode }) => {
       port: 3000,
       host: "0.0.0.0",
     },
-    // Gabungkan kedua plugin di sini
-    plugins: [react(), vercel()],
+    plugins: [react(), netlify()],
     define: {
       "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
       "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
